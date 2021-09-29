0087df06f61532bca2c432997e1b66575469ecc5
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-09-29 15:50:21 -0400

    modal opens large picture

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-// import Modal from "../Modal";
+import Modal from "../Modal";
 
 const PhotoList = ({ category }) => {
-  // const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentPhoto, setCurrentPhoto] = useState();
 
   const [photos] = useState([
@@ -124,23 +124,21 @@ const PhotoList = ({ category }) => {
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
 
-  // const toggleModal = (image, i) => {
-  //   setCurrentPhoto({ ...image, index: i });
-  //   setIsModalOpen(!isModalOpen);
-  // };
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(true);
+  };
 
   return (
     <div>
-      {/* {isModalOpen && (
-        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
-      )} */}
+      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
             src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
-            // onClick={() => toggleModal(image, i)}
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
